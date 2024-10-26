9dc67585f79bbc988e262797c740ec29eb3b1927
Madhu Ds <madhuds140@gmail.com>
2024-10-26 21:10:18 +0530

    first

---
+++ Documents/Mutiple-terraform.tfvars/variable.tf
@@ -0,0 +1,25 @@
+variable "aws_instance" {
+  description = "aws-instance"
+  type        = string
+}
+variable "region" {
+  description = "region"
+  type        = string
+}
+variable "access_key" {
+  description = "access_key"
+  type        = string
+}
+variable "secret_key" {
+  description = "secret_key"
+  type        = string
+
+}
+variable "key_name" {
+  description = "key_name"
+  type        = string
+}
+#variable "vpc_security_group_id" {
+#  description = "vpc_security_group_id"
+#  type = string
+#}
